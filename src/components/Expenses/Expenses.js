c3ec5a5c637b38168418cfebe4de0e892a86842f
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-15 09:05:20 +0700

    rendering conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
@@ -13,25 +13,13 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  let expenseContent = <div>No expense found</div>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((item) => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        date={item.date}
-        amount={item.amount}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         value={filteredYear}
         onChangeFilter={onChangeFilterHandler}
       />
-      {expenseContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
