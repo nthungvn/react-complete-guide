7a194d4991f5f664b8fba56fd536887571252584
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-17 11:21:36 +0700

    use react portals to move element out

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,51 @@
+import { Fragment } from 'react';
+import ReactDom from 'react-dom';
 import Button from '../Button/Button';
 import Card from '../Card/Card';
 import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return (
+    <div
+      onClick={props.onConfirm}
+      className={`${classes.backdrop} ${classes[props.visible]}`}
+    />
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={`${classes.modal} ${classes[props.visible]}`}>
+      <header className={classes.header}>
+        <h3>{props.title}</h3>
+      </header>
+      <div className={classes.content}>{props.message}</div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const Modal = (props) => {
   const visible = props.visible === 'true' ? 'show' : 'hide';
 
   return (
-    <div className={classes[visible]}>
-      <div onClick={props.onConfirm} className={classes.backdrop}></div>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h3>{props.title}</h3>
-        </header>
-        <div className={classes.content}>{props.message}</div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onConfirm={props.onConfirm} visible={visible} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          onConfirm={props.onConfirm}
+          title={props.title}
+          message={props.message}
+          visible={visible}
+        />,
+        document.getElementById('modal-overlay-root')
+      )}
+    </Fragment>
   );
 };
 
