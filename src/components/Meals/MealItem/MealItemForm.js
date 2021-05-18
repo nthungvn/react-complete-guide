a9aa6031e53320527b522c54785de8eea4564cc1
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-18 20:11:55 +0700

    add input form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,20 @@
+import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const inputProps = {
+    id: `amount-${props.id}`,
+    name: 'amount',
+    type: 'number',
+    min: '1',
+    max: '5',
+    step: '1',
+    defaultValue: '1',
+  };
+
   return (
     <form className={classes.form}>
-      <div>
-        <label>Amount</label>
-        <input type="number" />
-      </div>
+      <Input input={inputProps} label="Amount" />
       <button>+ Add</button>
     </form>
   );
