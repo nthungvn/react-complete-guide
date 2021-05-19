143aa366331ac6ffe3163084f84086a8d3a80b34
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-19 17:29:57 +0700

    use effect to add visualize to button cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
   const numberOfCartItems = cartCtx.items.reduce(
     (prev, item) => prev + item.amount,
     0
   );
 
+  let buttonClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+
+  useEffect(() => {
+    if (numberOfCartItems === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [numberOfCartItems]);
+
   return (
-    <button
-      className={`${classes.button} ${classes.bump}`}
-      onClick={props.onClick}
-    >
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
