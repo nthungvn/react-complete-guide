71a1cf83719f5de68b4026f1ef9f204b6c5dfee2
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-19 16:07:12 +0700

    add item to cart and implemnt forward ref

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,11 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
   const inputProps = {
     id: `amount-${props.id}`,
     name: 'amount',
@@ -12,10 +16,26 @@ const MealItemForm = (props) => {
     defaultValue: '1',
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value.trim();
+    const numberEnteredAmount = +enteredAmount;
+    if (
+      enteredAmount.length === 0 ||
+      numberEnteredAmount < 1 ||
+      numberEnteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(numberEnteredAmount);
+  };
+
   return (
-    <form className={classes.form}>
-      <Input input={inputProps} label="Amount" />
-      <button>+ Add</button>
+    <form className={classes.form} onClick={submitHandler}>
+      <Input ref={amountInputRef} input={inputProps} label="Amount" />
+      <button type="submit">+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
