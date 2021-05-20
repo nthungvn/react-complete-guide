7a860103af39b003b12dd6bf33958af700505a11
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-20 11:29:36 +0700

    component update in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
+import React, { useCallback, useState } from 'react';
 import './App.css';
+import Button from './components/UI/Button/Button';
+
 
 function App() {
+  console.log('APP RUNNING...');
+  const [showParagraph, setShowParagraph] = useState(false);
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevState) => !prevState);
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
