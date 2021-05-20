e28cd2e2c51fa813240ed2548686fc847b4cb905
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-20 14:37:57 +0700

    toggle still not update due to handler doesn't change

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import Button from './components/UI/Button/Button';
 import OutputDemo from './components/Demo/OutputDemo';
 
-
 function App() {
   console.log('APP RUNNING...');
   const [showParagraph, setShowParagraph] = useState(false);
+  const [enableToggle, setEnableToggle] = useState(false);
+
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevState) => !prevState);
+    if (enableToggle) {
+      setShowParagraph((prevState) => !prevState);
+    }
+  }, []);
+
+  const enableToggleHandler = useCallback(() => {
+    setEnableToggle(true);
   }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <OutputDemo show={true}>This is new!</OutputDemo>
+      <OutputDemo show={showParagraph}>This is new!</OutputDemo>
+      <Button onClick={enableToggleHandler}>Enable Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
