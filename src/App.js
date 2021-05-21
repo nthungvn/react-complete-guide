c5cd42662335f2a0635d12e398d7cf9aae310828
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-21 16:34:44 +0700

    another way to use combine useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import MoviesList from './components/MoviesList';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch('https://swapi.dev/api/films/');
-        if (!response.ok) {
-          throw new Error('Something went wrong');
-        }
-        const data = await response.json();
-        const movies = data.results.map((movie) => ({
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        }));
-        setMovies(movies);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (isLoading) {
-      fetchMovies();
-    }
-  }, [isLoading]);
-
-  const fetchMoviesHandler = () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-  };
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await response.json();
+      const movies = data.results.map((movie) => ({
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      }));
+      setMovies(movies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>;
 
