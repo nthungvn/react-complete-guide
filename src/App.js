8d1ec931141987d585bb4865b298b9ec2bc03d54
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-21 16:30:53 +0700

    useEffect for loading data after rendering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MoviesList from './components/MoviesList';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async () => {
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch('https://swapi.dev/api/films/');
+        if (!response.ok) {
+          throw new Error('Something went wrong');
+        }
+        const data = await response.json();
+        const movies = data.results.map((movie) => ({
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        }));
+        setMovies(movies);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (isLoading) {
+      fetchMovies();
+    }
+  }, [isLoading]);
+
+  const fetchMoviesHandler = () => {
     setIsLoading(true);
     setError(null);
-    try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Something went wrong');
-      }
-      const data = await response.json();
-      const movies = data.results.map((movie) => ({
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      }));
-      setMovies(movies);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
   };
 
   let content = <p>Found no movies</p>;
