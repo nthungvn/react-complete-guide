04a1ab2c163a81bc95de5f88d053ef4556542712
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-21 17:10:47 +0700

    send a post request to create movie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 
 function App() {
@@ -11,23 +12,40 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-complete-guide-400e6-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json'
+      );
       if (!response.ok) {
         throw new Error('Something went wrong');
       }
       const data = await response.json();
-      const movies = data.results.map((movie) => ({
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      }));
+      const movies = [];
+      for (let key in data) {
+        const movie = data[key];
+        movies.push({
+          id: key,
+          ...movie,
+        });
+      }
       setMovies(movies);
     } catch (error) {
       setError(error.message);
     } finally {
       setIsLoading(false);
     }
+  }, []);
+
+  const addMovieHandler = useCallback(async (movie) => {
+    const response = await fetch(
+      'https://react-complete-guide-400e6-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(movie),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }, []);
 
   useEffect(() => {
@@ -50,6 +68,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
