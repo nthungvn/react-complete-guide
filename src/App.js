d1ba93a8ccb140b55d7fd686e6f4b57337814ee3
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-21 13:50:31 +0700

    useContext in class-based

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const userContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
+    <UsersContext.Provider value={userContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
