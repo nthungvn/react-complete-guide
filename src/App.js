834be2bc5f821dd7ff5ba26555aba0d917910622
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-25 17:45:01 +0700

    use the second slice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth);
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
