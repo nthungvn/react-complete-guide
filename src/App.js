4c25280b7e46b2838731cc124434f724008c7c8e
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-25 15:13:34 +0700

    use redux in class-based

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Counter from './components/Counter';
-
+import CounterClass from './components/CounterClass';
 
 function App() {
-  return (
-    <Counter />
-  );
+  return <CounterClass />;
 }
 
 export default App;
