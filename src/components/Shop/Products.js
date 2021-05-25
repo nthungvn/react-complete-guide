4481bf212d23256849c5e95b9803b1d65ecfc431
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-25 20:03:35 +0700

    add more dummy project

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,17 +1,42 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    title: 'Test',
+    price: 6,
+    description: 'This is a first product - amazing!',
+  },
+  {
+    id: 'p2',
+    title: 'Test 2',
+    price: 16,
+    description: 'This is a first product - amazing!',
+  },
+  {
+    id: 'p3',
+    title: 'Test 3',
+    price: 13.99,
+    description: 'This is a first product - amazing!',
+  },
+];
+
 const Products = (props) => {
+  const productList = DUMMY_PRODUCTS.map((product) => (
+    <ProductItem
+      key={product.id}
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      description={product.description}
+    />
+  ));
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
+      <ul>{productList}</ul>
     </section>
   );
 };
