5c63e93f54dab938ebbdd001a2dd45bc3feaa6b8
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-25 17:46:04 +0700

    split the slice to their own file

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,6 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './auth';
-
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.counter++;
-    },
-    decrement: (state) => {
-      state.counter--;
-    },
-    increase: (state, action) => {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter: (state) => {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
+import { counterSlice } from './counter';
 
 const store = configureStore({
   reducer: {
