874b55378de114dd6d127cc6cb9d02e8915d7a37
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-25 14:07:43 +0700

    redux in nutshell

---
+++ redux-demo.js
@@ -0,0 +1,26 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const currentState = store.getState();
+  console.log(currentState);
+};
+
+store.subscribe(counterSubscriber);
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
