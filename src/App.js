bc99fd72ca063238da04ddc7ed854e493b189eb4
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-26 15:50:16 +0700

    use the action creator thunk to move logic to slice

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -15,60 +15,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      try {
-        const response = await fetch(
-          'https://react-complete-guide-400e6-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-          {
-            method: 'PUT',
-            body: JSON.stringify(cart),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        );
-        if (!response.ok) {
-          throw new Error('Something went wrong!');
-        }
-        dispatch(
-          uiActions.showNotification({
-            status: 'success',
-            title: 'Success!',
-            message: 'Sending cart data successfully',
-          })
-        );
-      } catch (error) {
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            message: error.message,
-            title: 'Error!',
-          })
-        );
-      }
-      setTimeout(() => {
-        dispatch(
-          uiActions.showNotification({
-            status: undefined,
-            message: undefined,
-            title: undefined,
-          })
-        );
-      }, 2000);
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData();
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const initialState = {
   items: [],
@@ -33,6 +34,57 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    );
+    try {
+      const response = await fetch(
+        'https://react-complete-guide-400e6-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sending cart data successfully',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          message: error.message,
+          title: 'Error!',
+        })
+      );
+    }
+    setTimeout(() => {
+      dispatch(
+        uiActions.showNotification({
+          status: undefined,
+          message: undefined,
+          title: undefined,
+        })
+      );
+    }, 2000);
+  };
+};
 
 export const cartReducer = cartSlice.reducer;
 export const cartActions = cartSlice.actions;
