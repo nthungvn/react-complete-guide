8732441317401cb54d4bf209b60d1979f969f9e8
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 11:20:04 +0700

    use the Switch and exact flag

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+import classes from './Products.module.css';
+
 const Products = (props) => {
-  return <h2>The Products page</h2>;
+  return (
+    <section className={classes.products}>
+      <h2>The Products page</h2>
+      <ul>
+        <li>
+          <Link to="/products/book">A book</Link>
+        </li>
+        <li>
+          <Link to="/products/carpet">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/online-course">An online course</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
