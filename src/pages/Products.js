9061894f2621497aa4a90bde5d966ac368dc1219
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 08:32:27 +0700

    define and use routes

---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const Products = (props) => {
+  return <h2>The Products page</h2>;
+};
+
+export default Products;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,6 @@
+
+const Welcome = (props) => {
+  return <h2>The Welcome page</h2>
+};
+
+export default Welcome;
