139b53c639a4e4e2947bf3ce540d8f3a94a88bb6
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 08:57:06 +0700

    working with links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Route } from 'react-router';
+import MainHeader from './components/MainHeader';
 import Products from './pages/Products';
 import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path={['/welcome', '/']}>
+          <Welcome />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const MainHeader = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/welcome">Welcome</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MainHeader;
