13490367455ed55b67574eca11eb635a6ddeac0f
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 16:26:34 +0700

    add page not found

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/layout/Layout';
 import MainNavigation from './components/layout/MainNavigation';
 import AllQuotes from './pages/AllQuotes';
 import NewQuote from './pages/NewQuote';
+import NotFound from './pages/NotFound';
 import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
@@ -23,6 +24,9 @@ function App() {
           </Route>
           <Route path="/quotes/:quoteId">
             <QuoteDetail />
+          </Route>
+          <Route>
+            <NotFound />
           </Route>
         </Switch>
       </Layout>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import classes from './NotFound.module.css';
+
+const NotFound = (props) => {
+  return <h2 className={classes['not-found']}>Page Not Found</h2>;
+};
+
+export default NotFound;
