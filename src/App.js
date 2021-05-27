da57fd37af75a4df77db00a4bb902687c4f11806
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 15:58:38 +0700

    add the routes and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Fragment } from 'react';
+import { Route, Switch } from 'react-router';
+import Layout from './components/layout/Layout';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      <MainNavigation />
+      <Layout>
+        <Switch>
+          <Route path="new-quotes"></Route>
+          <Route path="quotes"></Route>
+          <Route path="quotes/:quoteId"></Route>
+        </Switch>
+      </Layout>
+    </Fragment>
   );
 }
 
