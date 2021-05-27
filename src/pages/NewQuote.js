dc0932215697b9baf38879f6745c79184bd4453d
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 17:32:38 +0700

    move user to quotes after adding quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = (props) => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
