48b646dd4e42b8a86cdd4e376fc17e9cc9ff4391
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 16:51:09 +0700

    add the nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,17 @@
-import { useParams } from "react-router";
+import { Route, useParams, useRouteMatch } from "react-router";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = (props) => {
   const params = useParams();
+  const match = useRouteMatch();
 
   return (
     <section>
       <h2>Quote Detail Pages</h2>
       <p>{params.quoteId}</p>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
     </section>
   );
 };
