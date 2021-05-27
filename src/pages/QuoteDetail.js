4be8831e33e62aa87ff740a22b2d446a388d3e56
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 17:15:45 +0700

    update content of quote detail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,37 @@
-import { Route, useParams, useRouteMatch } from "react-router";
-import Comments from "../components/comments/Comments";
+import { Route, useParams, useRouteMatch } from 'react-router';
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Hung',
+    text: 'Learning new thing',
+  },
+  {
+    id: 'q2',
+    author: 'Phuoc',
+    text: 'Learning new thing',
+  },
+];
 
 const QuoteDetail = (props) => {
   const params = useParams();
   const match = useRouteMatch();
 
+  const selectedQuote = DUMMY_QUOTES.find(
+    (quote) => quote.id === params.quoteId
+  );
+  if (!selectedQuote) {
+    return <h2>No Quote Found</h2>;
+  }
+
   return (
     <section>
-      <h2>Quote Detail Pages</h2>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote
+        author={selectedQuote.author}
+        text={selectedQuote.text}
+      />
       <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
