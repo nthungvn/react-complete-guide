5d9ab4f82da7dd6459a7616056b6fa734564ad90
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-27 22:03:35 +0700

    add the new quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,15 +1,25 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = (props) => {
+  const { sendRequest: addQuoteRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
 
-    history.push('/quotes');
+  const addQuoteHandler = (quoteData) => {
+    addQuoteRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
+  );
 };
 export default NewQuote;
