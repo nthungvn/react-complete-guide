1ce9551593865e6b2591c139cf6bb946da5f6712
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-28 09:15:10 +0700

    add lazyloading

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import { Fragment } from 'react';
+import React, { Fragment, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import Layout from './components/layout/Layout';
 import MainNavigation from './components/layout/MainNavigation';
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
-import QuoteDetail from './pages/QuoteDetail';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
 
 function App() {
+  const fallback = (
+    <div className="centered">
+      <LoadingSpinner />
+    </div>
+  );
+
   return (
     <Fragment>
       <MainNavigation />
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/new-quote">
-            <NewQuote />
-          </Route>
-          <Route exact path="/quotes">
-            <AllQuotes />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense fallback={fallback}>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/new-quote">
+              <NewQuote />
+            </Route>
+            <Route exact path="/quotes">
+              <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+              <QuoteDetail />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </Fragment>
   );
