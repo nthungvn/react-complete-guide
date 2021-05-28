3fb2b858c825e0cb901c7df048df3dce81136c8e
Hung Nguyen <nthung.vlvn@gmail.com>
2021-05-28 14:12:11 +0700

    change password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,41 @@
+import { useEffect, useRef } from 'react';
+import useHttp from '../../hooks/use-http';
+import { changePassword } from '../../libs/firebase-api';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const passwordInputRef = useRef();
+  const { sendRequest, data, status } = useHttp(changePassword);
+
+  console.log(data, status);
+
+  const changePasswordHandler = (event) => {
+    event.preventDefault();
+    const password = passwordInputRef.current.value.trim();
+    if (!password || password.length < 6) {
+      return;
+    }
+    const idToken = localStorage.getItem('token');
+    sendRequest({ password, idToken });
+  };
+
+  useEffect(() => {
+    if (data && status === 'completed') {
+      localStorage.setItem('token', data.idToken);
+    }
+  }, [data, status]);
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={changePasswordHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={passwordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
--- src/libs/firebase-api.js
+++ src/libs/firebase-api.js
@@ -42,3 +42,24 @@ export const signIn = async (userData) => {
 
   return data;
 };
+
+export const changePassword = async (userData) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${firebaseKey}`;
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      ...userData,
+      returnSecureToken: true,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Cannot change password!')
+  }
+
+  return data;
+};
