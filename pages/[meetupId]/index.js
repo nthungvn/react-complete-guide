80be098efafce9c11d46372c3ace3784391565dc
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-02 17:36:29 +0700

    add custom component and style it

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,16 @@
-const MeetupDetailPage = (props) => {
-  return <div>Detail Page</div>;
+import MeetupDetail from '../../components/meetups/MeetupDetails';
+
+const MeetupDetailsPage = (props) => {
+  const data = {
+    id: 'm1',
+    title: 'Learn Next.js together',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/640px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+    address: 'Munich',
+    description: 'A great place that we can share our knowledge',
+  };
+
+  return <MeetupDetail {...data} />;
 };
 
-export default MeetupDetailPage;
+export default MeetupDetailsPage;
