856f68ae3bf27c1afb48d898ec85676a3cba7ac0
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-02 15:49:37 +0700

    extract dynamic query parameter

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,5 +1,15 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+
 const NewsDetail = (props) => {
-  return <h1>The New Detail Page</h1>
+  const router = useRouter();
+
+  return (
+    <Fragment>
+      <h1>The New Detail Page</h1>
+      <h3>{router.query.newsId}</h3>
+    </Fragment>
+  );
 };
 
 export default NewsDetail;
