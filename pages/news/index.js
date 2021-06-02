4ca0ea9a20fc4b874a6b50bc947aceb9f5f441fe
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-02 15:52:42 +0700

    link pages with Link component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 const News = (props) => {
-  return <h1>The News Page</h1>
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">The Next.js is great</Link>
+        </li>
+        <li>
+          <Link href="/news/another-news">Another News</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default News;
