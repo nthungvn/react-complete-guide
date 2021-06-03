74860807258af1e427a3d3dbf0a6e5a84455b891
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-03 14:52:19 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
+import Modal from './components/Modal/Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal />
+        <Backdrop />
+        <button className="Button">Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
 }
 
 export default App;
