aa1f6e5736bea9b3d3cb14ac5c0e8a05ff8bc734
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-03 10:54:36 +0700

    update code call rest from client to server

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,15 +1,20 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetUpPage = (props) => {
-  const addMeetupHandler = (meetupData) => {
+  const router = useRouter();
+
+  const addMeetupHandler = async (meetupData) => {
     console.log(meetupData);
-    fetch('/api/new-meetup', {
+    const response = await fetch('/api/new-meetup', {
       method: 'POST',
       body: JSON.stringify(meetupData),
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    console.log(response.json());
+    router.replace('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
