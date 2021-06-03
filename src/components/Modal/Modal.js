d6d2d67db9f916bb6e13cfabfee0b95ef6a0848a
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-03 21:12:53 +0700

    use the correct state to conditional adding style

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,16 +3,20 @@ import { Transition } from 'react-transition-group';
 import './Modal.css';
 
 const modal = (props) => {
-  const show = !!props.show;
-
   return (
-    <Transition in={show} timeout={400} mountOnEnter unmountOnExit>
-      <div className={`Modal ${show ? 'Open' : 'Close'}`}>
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>
-          Dismiss
-        </button>
-      </div>
+    <Transition in={props.show} timeout={400} mountOnEnter unmountOnExit>
+      {(state) => (
+        <div
+          className={`Modal ${
+            state === 'entering' ? 'Open' : state === 'exiting' ? 'Close' : ''
+          }`}
+        >
+          <h1>A Modal</h1>
+          <button className="Button" onClick={props.closed}>
+            Dismiss
+          </button>
+        </div>
+      )}
     </Transition>
   );
 };
