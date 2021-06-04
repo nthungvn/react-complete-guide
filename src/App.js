bf058e7c40d1f2143f799208ab2b3663212a12b5
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-04 12:12:54 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import Navigation from './components/Nav/Navigation';
+import FavoritesPage from './containers/Favorites';
+import ProductsPage from './containers/Products';
+
+const App = (props) => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Navigation />
+      <main>
+        <Route path="/" component={ProductsPage} exact />
+        <Route path="/favorites" component={FavoritesPage} />
+      </main>
+    </Fragment>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { combineReducers, createStore } from 'redux';
 import App from './App';
+import './index.css';
+import productReducer from './store/reducers/products';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  shop: productReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
