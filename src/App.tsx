134f811253f0af9a925f18d4764aebf64a262e47
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-06 06:43:57 +0700

    add the first typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import Todos from './components/Todos';
+
+const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Todos />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,10 @@
+const Todos: React.FC = (props) => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn Typescript</li>
+    </ul>
+  );
+};
+
+export default Todos;
