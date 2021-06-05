d65a260bf55290997725f207f417a96c3bf0dc26
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-06 06:50:07 +0700

    add data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Todos from './components/Todos';
+import { Todo } from './models/todo';
 
 const App = () => {
-  const todos: string[] = ['Learn React', 'Learn Typescript'];
+  const todos: Todo[] = [
+    { id: 't1', text: 'Learn React' },
+    { id: 't1', text: 'Learn Typescript' },
+  ];
 
   return (
     <div>
