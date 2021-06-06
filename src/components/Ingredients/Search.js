2b0232c6391883785827e02f4ea8a84c9ab9b6d8
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-06 17:21:56 +0700

    add timer boundary for prevent duplication send http request

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Card from '../UI/Card';
 import './Search.css';
 
@@ -7,27 +7,37 @@ const url =
 
 const Search = React.memo((props) => {
   const [enteredSearchText, setEnteredSearchText] = useState('');
+  const searchTextInputRef = useRef();
   const onSearch = props.onSearch;
 
   const changeSearchTextHandler = (event) => {
-    const searchText = event.target.value.trim();
+    const searchText = event.target.value;
     setEnteredSearchText(searchText);
   };
 
   useEffect(() => {
-    const query = enteredSearchText
-      ? `?orderBy="title"&equalTo="${enteredSearchText}"`
-      : '';
-    const ingredients = [];
-    fetch(url + query)
-      .then((response) => response.json())
-      .then((data) => {
-        for (let key in data) {
-          const ingredient = data[key];
-          ingredients.push({ id: key, ...ingredient });
-        }
-        onSearch(ingredients);
-      });
+    let timer;
+    timer = setTimeout(() => {
+      if (enteredSearchText === searchTextInputRef.current.value) {
+        const query = enteredSearchText
+          ? `?orderBy="title"&equalTo="${enteredSearchText}"`
+          : '';
+        const ingredients = [];
+        fetch(url + query)
+          .then((response) => response.json())
+          .then((data) => {
+            for (let key in data) {
+              const ingredient = data[key];
+              ingredients.push({ id: key, ...ingredient });
+            }
+            onSearch(ingredients);
+          });
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredSearchText, onSearch]);
 
   return (
@@ -37,8 +47,9 @@ const Search = React.memo((props) => {
           <label>Filter by Title</label>
           <input
             type="text"
-            // value={enteredSearchText}
+            value={enteredSearchText}
             onChange={changeSearchTextHandler}
+            ref={searchTextInputRef}
           />
         </div>
       </Card>
