c2d84dd5380c964a043e74469ee879971f713a28
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-07 17:51:00 +0700

    apply favorites context to favorites page

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,5 +1,21 @@
+import { Fragment, useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+import { FavoritesContext } from '../store/favorites-context';
+
 const FavoritesPage = (props) => {
-  return <h1>The Favorites Page</h1>;
+  const favoritesCtx = useContext(FavoritesContext);
+  const meetups = favoritesCtx.favorites;
+  let content = <p>There is not favorites meetups yet</p>;
+  if (meetups.length > 0) {
+    content = <MeetupList meetups={meetups} />;
+  }
+
+  return (
+    <Fragment>
+      <h1>The Favorites Page</h1>
+      {content}
+    </Fragment>
+  );
 };
 
 export default FavoritesPage;
