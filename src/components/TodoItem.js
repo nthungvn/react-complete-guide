c87c18d92273a5f578f24d92d500b9d3e8c7a060
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-07 15:25:02 +0700

    handling events

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,15 @@
 const TodoItem = (props) => {
+  const deleteHandler = () => {
+    console.log('clicked on', props.text);
+  };
+
   return (
     <li className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
       </div>
     </li>
   );
