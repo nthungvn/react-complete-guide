abf6e4e2ac850fc391c5e6121b2cc0960b74c8c4
Hung Nguyen <nthung.vlvn@gmail.com>
2021-06-07 15:12:17 +0700

    work with component and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Todos from "./components/Todos";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>My Todos</h1>
+      <Todos />
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,20 @@
+const Todos = () => {
+  return (
+    <ul>
+      <li className="card">
+        <h2>Learn React Native</h2>
+        <div className="actions">
+          <button className="btn">DELETE</button>
+        </div>
+      </li>
+      <li className="card">
+        <h2>Learn React Master</h2>
+        <div className="actions">
+          <button className="btn">DELETE</button>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default Todos;
